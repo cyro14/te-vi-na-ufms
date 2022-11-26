a9e22fe4d3b51ea157762d1a7a5896934fcae05c
Davi Nogueira <87454234+DaviLNogueira@users.noreply.github.com>
2022-11-26 17:22:53 -0400

    tirar foto

--- app/src/main/java/com/example/myapplication/fragments/Add.java
+++ app/src/main/java/com/example/myapplication/fragments/Add.java
@@ -1,25 +1,59 @@
 package com.example.myapplication.fragments;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.myapplication.R;
+import com.example.myapplication.databinding.FragmentAddBinding;
+
+import java.net.URL;
 
 public class Add extends Fragment {
 
-    public Add() {
-        // Required empty public constructor
-    }
+    private FragmentAddBinding binding;
+    URL imagURL;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false);
+
+
+        binding = FragmentAddBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+     private static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,REQUEST_IMAGE_CAPTURE);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            binding.imageView2.setImageBitmap(imageBitmap);
+        }
     }
 }
