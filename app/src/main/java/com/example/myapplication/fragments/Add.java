74e45ac8c414b942ce7ad47514546c94188f3002
Davi Nogueira <87454234+DaviLNogueira@users.noreply.github.com>
2022-11-27 19:23:35 -0400

    add foto

--- app/src/main/java/com/example/myapplication/fragments/Add.java
+++ app/src/main/java/com/example/myapplication/fragments/Add.java
@@ -1,8 +1,10 @@
 package com.example.myapplication.fragments;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -14,16 +16,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentAddBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class Add extends Fragment {
 
     private FragmentAddBinding binding;
-    URL imagURL;
+    Uri imagURL;
+    StorageReference storageReference;
+    ProgressDialog progressDialog;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -46,14 +61,58 @@ public class Add extends Fragment {
                 startActivityForResult(intent,REQUEST_IMAGE_CAPTURE);
             }
         });
+        binding.postar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                progressDialog = new ProgressDialog(getContext());
+                progressDialog.setTitle("Uploading File....");
+                progressDialog.show();
+
+
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.CANADA);
+                Date now = new Date();
+                String fileName = formatter.format(now);
+                storageReference = FirebaseStorage.getInstance().getReference("images/"+fileName);
+
+
+                storageReference.putFile(imagURL).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Toast.makeText(getActivity(),"post publicado",Toast.LENGTH_SHORT).show();
+                        if (progressDialog.isShowing()){
+                            progressDialog.dismiss();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getActivity(),"post publicado",Toast.LENGTH_SHORT).show();
+                        if (progressDialog.isShowing()){
+                            progressDialog.dismiss();
+                        }
+                        Toast.makeText(getActivity(),"Não foi possível realizar a publicação",Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && data != null ) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imagURL = getImageUri(getActivity().getApplicationContext(),imageBitmap);
             binding.imageView2.setImageBitmap(imageBitmap);
         }
+    }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
     }
 }
